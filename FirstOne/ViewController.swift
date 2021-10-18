95d6525c6ed6f93380da33d18063ee1aaa332791
Alexander Tepper <alextepper92@gmail.com>
2021-10-18 16:21:31 +0300

    Initial Commit

---
+++ FirstOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstOne
+//
+//  Created by Alexander Tepper on 18/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
